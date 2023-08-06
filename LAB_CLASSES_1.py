e82402a2cf424265bc6bc93737abfc62b71a3969
naif <naif.albariqi201@hotmail.com>
2023-08-06 12:35:45 +0300

    lap1

---
+++ LAB_CLASSES_1.py
@@ -0,0 +1,18 @@
+class Panda():
+    def __init__(self, species:str,age:int,wight:int,hight:int, fast_speed:int) -> None:
+        self.__name = species
+        self.__age = age
+        self.__wight = wight
+        self.__hight = hight
+        self.__FastSpeed = fast_speed
+    def eat(self):
+        print(f"The {self.__name} eat bamboo shoots and leaves")
+    def speed(self):
+        print(f"The {self.__name} the fast speed is : {self.__FastSpeed}")
+
+    
+
+
+panda1 = Panda("The gaint Panda",10,150,180,52)
+panda1.eat()
+panda1.speed()
