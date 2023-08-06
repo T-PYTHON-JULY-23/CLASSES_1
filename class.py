5bca2a9a20512274d6a83eaebad112d8926a6294
ghaida meshal <108289065+Ghaida111@users.noreply.github.com>
2023-08-06 13:15:15 +0300

    Update class.py

--- class.py
+++ class.py
@@ -18,7 +18,7 @@ class Panda:
 panda1=Panda("rock","male","130","100")
 panda2=Panda("cake","female","110","80")
 panda3=Panda("loly","female","108","70")
-panda3=Panda("light","male","160","120")
+panda4=Panda("light","male","160","120")
 
 print(f"panda name {panda1.name}")
 print(f"name of mother and his son is {panda2.birth('moon')}",)
