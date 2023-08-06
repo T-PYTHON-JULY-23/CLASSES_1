95170e10f89247c8a904f8d7103a528827a3626c
Khaled_Alghamdi <khaled.alghamdi0@hotmail.com>
2023-08-06 13:08:11 +0300

    LabClasses

---
+++ panda.py
@@ -0,0 +1,34 @@
+class Panda():
+
+    def __init__(self,hight:str,wight:float,name:str,age:int) -> None:
+        self.hight=hight
+        self.wight=wight
+        self.name=name
+        self.age=age
+    
+
+    def set_hight_wight(self,hight:float,wight:float):
+        self.hight=hight
+        self.wight=wight
+
+
+    def panda_say_hello(self):
+        
+        print(f'Hello im {self.name}')
+    
+
+first_panda=Panda(166,75,'first',5)
+secend_panda=Panda(200,90,'secend',7)
+
+
+
+print(first_panda.name,first_panda.age,first_panda.hight,first_panda.wight)
+print(secend_panda.name,secend_panda.age,secend_panda.hight,secend_panda.wight)
+
+first_panda.panda_say_hello()
+secend_panda.panda_say_hello()
+
+
+first_panda.set_hight_wight(secend_panda.hight,secend_panda.wight)
+
+print(first_panda.name,first_panda.age,first_panda.hight,first_panda.wight)
