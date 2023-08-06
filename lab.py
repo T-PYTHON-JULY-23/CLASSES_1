32ca8bf2e0bb2a94ec4109f7d74b834423b3340a
Nada Almutairi <nadaamusaad@gmail.com>
2023-08-06 14:10:53 +0300

    done

--- lab.py
+++ lab.py
@@ -8,7 +8,7 @@ class panda:
     def devouring_bamboo(self,meals):
         return (f"this panda is {self.color}, devouring bamboo {meals} in day")   
     
-    def sleep_hours(self,hours):
+    def sleep(self,hours):
         return (f"this panda is {self.color}, sleep hours {hours} in day")   
 
 panda1=panda("brown",20,180,113)
@@ -17,8 +17,8 @@ panda3=panda("black",10,120,105)
 panda4=panda("white & black",26,180,120)
 
 print(panda1.devouring_bamboo(5))
-print(panda2.sleep_hours(10))
+print(panda2.sleep(10))
 print(panda3.devouring_bamboo(3))
-print(panda4.sleep_hours(9))
+print(panda4.sleep(9))
 
 
